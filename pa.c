c052b8f806234cfa62e59fab7bf10744941b74a1
levarev <levonarev2@gmail.com>
2026-03-04 18:33:38 +0400

    Add files via upload

---
+++ pa.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pa.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: larevsha <larevsha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/04 14:58:13 by larevsha          #+#    #+#             */
+/*   Updated: 2026/03/04 18:32:48 by larevsha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_list
+{
+	int				num;
+	struct s_list	*next;
+} t_list;
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (!lst || !new)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
+
+void	pa(t_list **a, t_list **b)
+{
+	t_list	*temp;
+	if (!a || !b || !*b)
+		return ;
+	temp = *b;
+	*b = (*b)->next;
+	temp->next = NULL;
+	ft_lstadd_front(a, temp);
+}
+
+//int	main(void)
+//{
+//	t_list	*one = malloc(sizeof(t_list));
+//	t_list	*two = malloc(sizeof(t_list));
+//	t_list	*help;
+//	t_list	**a;
+//	t_list	**b;
+//	a = &one;
+//	b = &two;
+//	two->num = 5;
+//	two->next = NULL;
+//	one->num = 10;
+//	one->next = NULL;
+//	pa(a, b);
+//	help = one;
+//	while(help != NULL)
+//	{
+//		printf("%d\n", help->num);
+//		help = help->next;
+//	}
+//}
---
+++ rra.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rra.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: larevsha <larevsha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/04 16:33:09 by larevsha          #+#    #+#             */
+/*   Updated: 2026/03/04 18:21:20 by larevsha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_list
+{
+	int				num;
+	struct s_list	*next;
+} t_list;
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*a;
+
+	if (!lst)
+		return (NULL);
+	a = lst;
+	while (a->next)
+		a = a->next;
+	return (a);
+}
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (!lst || !new)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
+
+void	rra(t_list **a)
+{
+	t_list	*last;
+	t_list	*prev;
+
+	if (!a || !*a || !(*a)->next)
+		return ;
+	prev = *a;
+	last = ft_lstlast(*a);
+	while ((prev->next)->next != NULL)
+		prev = prev->next;
+	prev->next = NULL;
+	last->next = *a;
+	*a = last;
+}
+
+//int	main(void)
+//{
+//	t_list	*one = malloc(sizeof(t_list));
+//	t_list	*two = malloc(sizeof(t_list));
+//	t_list	*three = malloc(sizeof(t_list));
+//	t_list	*temp1;
+//	t_list	*temp2;
+//	t_list	**head;
+
+//	one->num = 5;
+//	one->next = two;
+//	two->num = 10;
+//	two->next = three;
+//	three->num = 15;
+//	three->next = NULL;
+//	head = &one;
+//	temp1 = *head;
+//	printf("minchev tpel\n");
+//	while(temp1 != NULL)
+//	{
+		
+//	}
+//	ra(head);
+//}
