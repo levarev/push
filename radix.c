a2829f39d95d9a8ecaa84aba4aaf19a233748256
levarev <levonarev2@gmail.com>
2026-03-19 21:44:48 +0400

    Add files via upload

---
+++ radix.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   radix.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: larevsha <larevsha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/19 21:36:12 by larevsha          #+#    #+#             */
+/*   Updated: 2026/03/19 21:42:31 by larevsha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.h"
+
+int	max_value(t_list **a, int size)
+{
+	
+}
+
+void	radix(t_list **a, t_list **b)
+{
+	int	size;
+	int	bit;
+	int	bits;
+	int	i;
+
+	i = 0;
+	bit = 0;
+	size = ft_lstsize(*a);
+	//INDEXAVOREL NOR SHARUNAKEL BITS
+	bits = max_value(a, size);
+}
